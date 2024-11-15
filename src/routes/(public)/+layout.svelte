b440c34beaf56dd2242bc2d99365556a8a2fba4b
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-11-15 17:58:55 -0600

    Add public index page and layout.

---
+++ src/routes/(public)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Header from "$lib/public/Header.svelte";
+    import Footer from "$lib/public/Footer.svelte";
+    import type { Snippet } from "svelte";
+
+    let { children }: { children: Snippet<[]> } = $props();
+</script>
+
+<Header />
+
+{@render children()}
+
+<Footer />
---
+++ src/routes/(public)/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Hero from "./_index/Hero.svelte";
+    import Stats from "./_index/Stats.svelte";
+    import Pricing from "./_index/Pricing.svelte";
+</script>
+
+<svelte:head>
+    <title>Upbeat</title>
+</svelte:head>
+
+<Hero />
+<Stats />
+<Pricing />
