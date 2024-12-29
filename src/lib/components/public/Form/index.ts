42deb58e9090e8d418e3e0a80f805b5cd762672d
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-12-29 01:27:30 -0600

    Add ContactForm export to Form component index

--- src/lib/components/public/Form/index.ts
+++ src/lib/components/public/Form/index.ts
@@ -3,3 +3,6 @@ export { default as Form } from "./Form.svelte";
 export { default as FormInput } from "./FormInput.svelte";
 export { default as FormRow } from "./FormRow.svelte";
 export { default as FormSubmit } from "./FormSubmit.svelte";
+
+// Prebuilt forms.
+export { default as ContactForm } from "./ContactForm.svelte";
