4fa4ad88acd5dcb9ac265871ffb4413b2cfe3485
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-12-29 01:30:03 -0600

    Refactor Section component to utilize PageSection and streamline props handling

--- src/routes/(public)/_index/Section.svelte
+++ src/routes/(public)/_index/Section.svelte
@@ -1,32 +1,7 @@
 <script lang="ts">
-    import type { Snippet } from "svelte";
+    import PageSection, { type props } from "$lib/components/public/PageSection.svelte";
 
-    let {
-        title,
-        text,
-        direction = "vertical",
-        height,
-        children,
-    }: {
-        title: string;
-        text: string;
-        direction?: "vertical" | "horizontal";
-        height?: string;
-        children?: Snippet<[]>;
-    } = $props();
+    let sectionProps: props = $props();
 </script>
 
-<div
-    class="flex-center mt-[50vh] {direction !== 'horizontal' ? 'flex-col' : ''} relative"
-    style={typeof height === "string" ? `height: ${height};` : ""}
->
-    <div class="flex-center relative z-10 mb-10 flex-col gap-3 text-center">
-        <p class="text-4xl font-bold lg:text-5xl">
-            {@html title}
-        </p>
-
-        <p class="text-light text-xl lg:max-w-4xl">{@html text}</p>
-    </div>
-
-    {@render children?.()}
-</div>
+<PageSection {...sectionProps} style={(sectionProps.style ?? "") + "margin-top: 50vh;"} />
