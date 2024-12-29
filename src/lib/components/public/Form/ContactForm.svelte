e3f71ee96d4de06c67b894a01caee86e7a7b5c5d
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-12-29 01:27:23 -0600

    Add ContactForm component with input fields for name, email, and message

---
+++ src/lib/components/public/Form/ContactForm.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Form, { type props } from "./Form.svelte";
+    import FormInput from "./FormInput.svelte";
+    import FormSubmit from "./FormSubmit.svelte";
+    import FormRow from "./FormRow.svelte";
+
+    let formProps: props = $props();
+</script>
+
+<Form {...formProps}>
+    <FormRow>
+        <FormInput id="name" label="Name" type="text" placeholder="John Doe" />
+        <FormInput id="email" label="Email" type="email" placeholder="johndoe@gmail.com" />
+    </FormRow>
+
+    <FormInput
+        id="message"
+        label="What are you contacting us about?"
+        type="textarea"
+        placeholder="Something important..."
+        classes="h-40"
+    />
+
+    <FormSubmit text="Send" />
+</Form>
