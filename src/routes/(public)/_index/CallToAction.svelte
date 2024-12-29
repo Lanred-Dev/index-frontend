a39dc92648dac29db210aca374f0112b4b4d18aa
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-12-29 01:29:17 -0600

    Fix import path for SpeedLines component and update CallToAction styling

--- src/routes/(public)/_index/CallToAction.svelte
+++ src/routes/(public)/_index/CallToAction.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
     import Section from "./Section.svelte";
-    import SpeedLines from "$lib/canvas/SpeedLines.svelte";
+    import SpeedLines from "$lib/components/canvas/SpeedLines.svelte";
 </script>
 
 <Section
     title="Stop waiting.<br />Create your portfolio today."
-    text="Register now and explore 30 days of Premium features at no cost. Available exclusively
-            for first-time users. Limited restrictions apply."
-    height="50vh"
+    text="Register now and explore 30 days of Premium features at no cost. Available exclusively for first-time users. Limited restrictions apply."
+    style="height: 50vh;"
 >
     <div class="flex-center gap-8">
         <a class="primary" id="download-button" href="/get-started">Create my portfolio</a>
