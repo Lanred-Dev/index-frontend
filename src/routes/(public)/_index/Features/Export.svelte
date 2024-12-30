549d65398c31d92ccacc0c95b6383492a0dd2373
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-12-30 14:04:45 -0600

    Add Export section

---
+++ src/routes/(public)/_index/Features/Export.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Section from "../Section.svelte";
+</script>
+
+<Section
+    title="Go beyond. Share your way."
+    text="Your portfolio deserves to be seen everywhere. With our export feature, you're not tied to the platform—transform your work into shareable images or, with premium, dynamic GIFs. Post them on social media, include them in emails, or showcase them in presentations. Share your story, your way, anywhere you choose."
+    direction="horizontal"
+></Section>
