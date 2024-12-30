24565bd9866edbecf38158dd46a574da3b4051e6
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-12-30 14:21:42 -0600

    Add Themes feature component

---
+++ src/routes/(public)/_index/Features/Themes.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Section from "../Section.svelte";
+</script>
+
+<Section
+    title="Your style. Your way."
+    text="Your portfolio should be as unique as your work. With our customizable themes, you don't need to worry about coding. Choose from a variety of sleek, professional templates and make them your own. Highlight your projects with colors, fonts, and layouts that match your style—no experience required. It's your portfolio, your rules."
+    direction="horizontal"
+></Section>
