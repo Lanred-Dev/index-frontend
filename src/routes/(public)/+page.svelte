8fe11f311f5cced5f6c41a1788210e8c1274bdfd
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-12-30 14:21:53 -0600

    Refactor imports in +page.svelte to include new Features components

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import Hero from "./_index/Hero.svelte";
-    import Testimonials from "./_index/Testimonials/List.svelte";
-    import Themes from "./_index/Themes.svelte";
+    import Testimonials from "./_index/Testimonials/Section.svelte";
+    import Themes from "./_index/Features/Themes.svelte";
+    import Export from "./_index/Features/Export.svelte";
+    import Discovery from "./_index/Features/Discovery/Section.svelte";
     import CallToAction from "./_index/CallToAction.svelte";
 </script>
 
@@ -15,5 +17,7 @@
 <span id="#features"></span>
 
 <Themes />
+<Export />
+<Discovery />
 
 <CallToAction />
