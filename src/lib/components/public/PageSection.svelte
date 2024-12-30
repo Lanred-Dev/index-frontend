caaef12a8b588ae77a27cf0dd728687a895fcb84
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-12-30 11:54:33 -0600

    Improve and fix horizontal rendering

--- src/lib/components/public/PageSection.svelte
+++ src/lib/components/public/PageSection.svelte
@@ -7,17 +7,34 @@
         direction?: "vertical" | "horizontal";
         classes?: string;
         style?: string;
+        wrapChildren?: boolean;
         children?: Snippet<[]>;
     };
 
-    let { title, text, direction = "vertical", classes, style, children }: props = $props();
+    let {
+        title,
+        text,
+        direction = "vertical",
+        classes,
+        style,
+        wrapChildren = true,
+        children,
+    }: props = $props();
+
+    let horizontal: boolean = direction === "horizontal";
 </script>
 
 <div
-    class="flex-center relative w-full {direction !== 'horizontal' ? 'flex-col' : ''} {classes}"
+    class="flex-center relative w-full {horizontal
+        ? 'pr-5 sm:pr-10 lg:justify-between'
+        : 'flex-col'} {classes}"
     {style}
 >
-    <div class="flex-center relative z-10 mb-10 flex-col gap-3 text-center">
+    <div
+        class="flex-center relative z-10 mb-10 flex-col gap-5 text-center {horizontal
+            ? 'w-full lg:w-[50%] lg:!items-start lg:pl-[10%] lg:text-left 2xl:pl-[15%]'
+            : ''}"
+    >
         <p class="text-4xl font-bold lg:text-5xl">
             {@html title}
         </p>
@@ -25,5 +42,11 @@
         <p class="text-light text-xl lg:max-w-4xl">{@html text}</p>
     </div>
 
-    {@render children?.()}
+    {#if horizontal && wrapChildren}
+        <div class="relative h-fit w-full lg:h-full lg:w-auto lg:flex-grow">
+            {@render children?.()}
+        </div>
+    {:else}
+        {@render children?.()}
+    {/if}
 </div>
